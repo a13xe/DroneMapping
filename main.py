78e12a040da77c40ead5d76e6c0effd864c97056
AlexeyLepov <77492646+AlexeyLepov@users.noreply.github.com>
2022-05-28 19:16:33 +0300

    Scaling option included

--- main.py
+++ main.py
@@ -16,22 +16,27 @@
 
 
 import cv2
-import shutil
 import imageMapper
 import dataProcesser
 import ImageMapperDynamic
 
 
-# Выбор метода
-method = 0
+method = 0 # Выбор метода
+scale_percent = 100 # Процент сжатия изображений.
 
 
 # Выполнение накидного монтажа для двух изображений
 if method == 0:
-    shutil.copyfile('data/2020_07_03_PhotoCamera_g401b40179_f001_028.JPG', 'res.JPG')
-    img1 = "data/2020_07_03_PhotoCamera_g401b40179_f001_039.JPG"
-    img2 = "res.JPG"
-    ImageMapperDynamic.stitchImages(img1, img2)
+    image_first_name = "data/2020_07_03_PhotoCamera_g401b40179_f001_025.JPG"
+    image_first = cv2.imread(image_first_name)
+    if scale_percent < 100:
+        dsize = (int(image_first.shape[1] * scale_percent / 100), int(image_first.shape[0] * scale_percent / 100))
+        image_first = cv2.resize(image_first, dsize)
+    cv2.imwrite("res.JPG",image_first)
+    # выполнение монтажа снимков по поступлению
+    img1 = "data/2020_07_03_PhotoCamera_g401b40179_f001_026.JPG"
+    img2 = "res.JPG"     
+    ImageMapperDynamic.stitchImages(img1, img2, scale_percent)
 
 
 # Выполнение накидного монтажа старым методом для всех снимков сразу
